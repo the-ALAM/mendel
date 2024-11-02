3c3ba276cc12ae70554b9d743ef4f18b00f92ea1
Mohamed Alameldeen <mohamed.alameldeen@nweave.com>
2024-11-02 04:19:29 +0200

    ✏️fix: modified the table names in the test queries

--- sql/test_queries.sql
+++ sql/test_queries.sql
@@ -10,15 +10,15 @@ from pg_indexes
 where schemaname <> 'pg_catalog' and schemaname <> 'information_schema';
 
 -- test querying patients' meds
-SELECT am.brand_name, mr.authored_on
-FROM medication_requests mr
+SELECT am.brand_name, mr.authoredon
+FROM src_tbl_medication_requests mr
 JOIN active_medications am ON mr.code = am.code
 WHERE mr.patient_id = 'c16b9aea-2b5f-3866-22a1-01dea645c9e1';
 
 -- test querying encounters
 SELECT e.id, e.patient_id, e.start, e.end, p.full_name
-FROM encounters e
-JOIN patients p ON e.patient_id = p.id
+FROM src_tbl_encounters e
+JOIN src_tbl_patients p ON e.patient_id = p.id
 WHERE e.start >= '2010-01-01' AND e.end <= '2020-01-01';
 
 -- test querying patients and encounters
