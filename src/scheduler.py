94f2d984eb69e780104f87b3305e30c68c555b84
Mohamed Alameldeen <mohamed.alameldeen@nweave.com>
2024-11-02 03:38:43 +0200

    ✨ add: a simple scheduler to to automate the ETL workflow by using the schedule package rather than falling into undifferentiated heavy lifting

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,2 +1,36 @@
 
-# TODO - (scheduling logic)
+import time
+import processor
+import loader
+import schedule
+
+def run_etl():
+    # TODO: Implement your ETL logic here
+    print("Running ETL tasks...")
+    try:
+        print("Starting data processing...")
+        processor.main()
+        print("Data processing (extraction and transformation) completed successfully")
+
+        time.sleep(10)
+
+        print("Starting data loading...")
+        loader.main()
+        print("Data loading completed successfully")
+
+        return True
+    except Exception as e:
+        print(f"ETL process failed: {e}")
+        return False
+
+def main():
+    """Main function.
+    """
+    schedule.every().day.at("02:00").do(run_etl)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(20)
+
+if __name__ == "__main__":
+    main()
